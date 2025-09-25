aad7d5dc3deb0d65807727308d3522332fab5067
MinhDucBui <minhducbui1996@yahoo.de>
2025-09-25 12:34:46 +0200

    add sentence

--- src/load_data.py
+++ src/load_data.py
@@ -14,7 +14,7 @@ def load_data(model_name):
         template_implicit = """{sentence}""" 
     else:
         template_explicit = """Translate the following English segment into Korean: 'I am addressing {addresse} and saying: {sentence}'\n\nProvide only the Korean translation, without any additional text or explanation."""
-        template_implicit = """Translate the following English segment into Korean: {sentence}\n\nProvide only the Korean translation, without any additional text or explanation."""
+        template_implicit = """Translate the following English segment into Korean: '{sentence}'\n\nProvide only the Korean translation, without any additional text or explanation."""
 
     df = pd.read_excel("data/results.xlsx", header=None)
 
